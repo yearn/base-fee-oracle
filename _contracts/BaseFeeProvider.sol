64ff37cf15d19b335004938a6a88c5a36c1db16c
dudesahn <23222916+dudesahn@users.noreply.github.com>
2022-11-01 12:17:53 -0400

    chore: more updates

--- _contracts/BaseFeeProvider.sol
+++ _contracts/BaseFeeProvider.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.15;
 
 /**
  * @dev Contract that reads the block base fee on supported
- * networks, including Ethereum, Fantom, and Arbitrum.
+ *  networks, including Ethereum, Fantom, and Arbitrum. Also
+8*  deployed on Optimism, but no base fee there yet.
  *
- * Version 0.1.0
+ *  Version 0.1.0
  */
 
 contract Basefee {
